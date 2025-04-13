806345e4201dca457b61dd4ba970261ea445c9f8
Noel Mathew <131687553+itsGBN@users.noreply.github.com>
2025-04-12 20:17:42 -0400

    Title Screen Created

---
+++ intothedeep/Assets/UI Toolkit/Title/Title.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class Title : MonoBehaviour
+{
+    private UIDocument uIDocument;
+    private VisualElement Trans;
+    private bool isTrans;
+
+    private void Awake()
+    {
+        //Refernce UI 
+        uIDocument = GetComponent<UIDocument>();
+        //Reference Visual Elements
+        Trans = uIDocument.rootVisualElement.Q("Trans") as VisualElement;
+        //Miscellaneous
+        isTrans = false;
+    }
+
+    private void Update()
+    {
+        if (Input.anyKeyDown && !isTrans)
+        {
+            isTrans = true;
+            StartCoroutine(StartTrans());
+        }
+    }
+
+    IEnumerator StartTrans()
+    {
+        Trans.style.opacity = 1;
+        yield return new WaitForSeconds(0.5f);
+        SceneManager.LoadScene(1);
+    }
+
+}
