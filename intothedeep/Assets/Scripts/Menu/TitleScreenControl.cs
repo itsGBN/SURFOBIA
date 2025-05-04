6d6bfaa512a982f9bb43cbf56dcfda8d061580aa
soeyzandiego <59988129+soeyzandiego@users.noreply.github.com>
2025-05-04 12:56:26 -0400

    title screen sounds, fungus stuff

--- intothedeep/Assets/Scripts/Menu/TitleScreenControl.cs
+++ intothedeep/Assets/Scripts/Menu/TitleScreenControl.cs
@@ -9,15 +9,20 @@ using UnityEngine.UI;
 public class TitleScreenControl : MonoBehaviour
 {
     [SerializeField] int sceneIndex = 1;
+    [SerializeField] Image fadeout;
+    [SerializeField] AudioClip[] sounds;
     private PS5Input GetInputs;
     private PlayableDirector director;
 
     bool playing = false;
+    int soundIndex = 0;
+    AudioSource source;
 
     private void Awake()
     {
         GetInputs = new PS5Input();
         director = FindObjectOfType<PlayableDirector>();
+        source = GetComponent<AudioSource>();
     }
 
     private void OnEnable()
@@ -54,12 +59,21 @@ public class TitleScreenControl : MonoBehaviour
 
     IEnumerator FadeOut()
     {
-        float alpha = 100;
-        while (alpha > 0)
+        float alpha = 0;
+        while (alpha < 1)
         {
-            alpha -= Time.deltaTime;
+            alpha += Time.deltaTime;
+            Color fadeoutC = fadeout.color;
+            fadeoutC.a = alpha;
+            fadeout.color = fadeoutC;
             yield return null;
         }
         SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void PlaySound()
+    {
+        source.PlayOneShot(sounds[soundIndex]);
+        if (soundIndex < sounds.Length) { soundIndex++; }
     }
 }
