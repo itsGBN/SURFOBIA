b632865c8570dd7e1b4b3a85165375875c50f927
Luis Lopez <lal8575@nyu.edu>
2025-05-09 04:47:53 -0400

    level three progress

--- intothedeep/Assets/Scripts/SpinObject.cs
+++ intothedeep/Assets/Scripts/SpinObject.cs
@@ -6,21 +6,21 @@ public class SpinObject : MonoBehaviour
     [SerializeField] private float _xRotation;
     [SerializeField] private float _yRotation;
     [SerializeField] private float _zRotation;
-    private Vector3 _currentRotationAngles;
+    private Vector3 _speedToRotateBy;
+    private Vector3 _currentAngles;
     private Quaternion _currentRotation;
     
     void Start()
     {
-        transform.rotation = Quaternion.identity;
-        transform.position = transform.position;
+        _currentAngles = transform.rotation.eulerAngles;
     }
 
     // Update is called once per frame
     void Update()
     {
-        _currentRotationAngles += new Vector3(_xRotation, _yRotation, _zRotation) * Time.deltaTime * _rotateSpeed;
+        _speedToRotateBy += new Vector3(_xRotation, _yRotation, _zRotation) * Time.deltaTime * _rotateSpeed;
 
-        _currentRotation.eulerAngles = _currentRotationAngles;
+        _currentRotation.eulerAngles = _currentAngles + _speedToRotateBy;
     }
 
     void FixedUpdate()
